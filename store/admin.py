9dd3855684c8d978114b16106864b807ce66057e
Me-r-tcan <mertcanbas35@gmail.com>
2022-02-04 21:16:13 +0300

    customize admin page listings

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,83 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
+from . import models
 
-# Register your models here.
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            })
+        )
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+    filter_parameter_low = '<10'
+    filter_parameter_ok = '>10'
+
+    def lookups(self, request, model_admin):
+        return [
+            (self.filter_parameter_low, 'Low'),
+            (self.filter_parameter_ok, 'Ok')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == self.filter_parameter_low:
+            return queryset.filter(inventory__lt=10)
+        elif self.value() == self.filter_parameter_ok:
+            return queryset.filter(inventory__gt=10)
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
+    list_display = ['title', 'price', 'inventory_status', 'collection_title']
+    list_editable = ['price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'Ok'
+
+    admin.action(description='Clear Inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were succesfully updated.',
+            messages.ERROR
+        )
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+
+
+admin.site.register(models.Promotion)
